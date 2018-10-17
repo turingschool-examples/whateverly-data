ed9085bfbc859f0e3a9964cf5c6b53cc248cb73a
Brittany Storoz <brittanystoroz@gmail.com>
2018-10-17 15:26:59 -0600

    Add sample data and endpoints

---
+++ datasets/alumni.js
@@ -0,0 +1,6 @@
+const students = [
+  { "id": 1, "name": "Bob", "cohort": 1610 },
+  { "id": 2, "name": "Pam", "cohort": 1710 }
+];
+
+module.exports = students;
---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const http = require('http').Server(app);
+const cors = require('express-cors');
+const bodyParser = require('body-parser');
+
+const students = require('./datasets/alumni.js');
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.set('port', process.env.PORT || 3000);
+
+app.get('/students', (request, response) => {
+  response.send({ students });
+});
+
+app.get('/students/:id', (request, response) => {
+  const { id } = request.params;
+  const student = students.find(student => student.id == id);
+  if (student) { return response.send(student); }
+  return response.sendStatus(404);
+});
+
+if (!module.parent) {
+  app.listen(app.get('port'), () => {
+    console.log(`Dataset is running on ${app.get('port')}.`);
+  });
+}
+
+module.exports = app;
