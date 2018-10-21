c2dc07c292a7828dea0e247aa8f4e61bb2d8a500
Justin Torrance <justin.torrance91@gmail.com>
2018-10-21 16:15:13 -0600

    Add coloradoHikes dataset

---
+++ datasets/coloradoHikes.js
@@ -0,0 +1,201 @@
+{
+  "coloradoHikes": {
+    "denver": [
+      {
+        "name": "Meadow and Forest Loop at Lookout Mountain Nature Center",
+        "difficulty": "easy",
+        "lengthInMiles": 0.8,
+        "distanceFromCityInMinutes": 30
+      },
+      {
+        "name": "Trading Post Trail at Red Rocks Park",
+        "difficulty": "easy",
+        "lengthInMiles": 1.4,
+        "distanceFromCityInMinutes": 25
+      },
+      {
+        "name": "Mills Lake",
+        "difficulty": "moderate",
+        "lengthInMiles": 5.3,
+        "distanceFromCityInMinutes": 200
+      },
+      {
+        "name": "Sleepy Lion Trail From Button Rock Preserve",
+        "difficulty": "moderate",
+        "lengthInMiles": 5.4,
+        "distanceFromCityInMinutes": 120
+      },
+      {
+        "name": "Centennial Cone Trail From Camino Perdido",
+        "difficulty": "difficult",
+        "lengthInMiles": 13.3,
+        "distanceFromCityInMinutes": 120
+      },
+      {
+        "name": "Centennial Cone Trail From Camino Perdido",
+        "difficulty": "difficult",
+        "lengthInMiles": 13.3,
+        "distanceFromCityInMinutes": 120
+      },
+      {
+        "name": "Centennial Cone Trail From Camino Perdido",
+        "difficulty": "difficult",
+        "lengthInMiles": 13.3,
+        "distanceFromCityInMinutes": 120
+      },
+      {
+        "name": "Centennial Cone Trail From Camino Perdido",
+        "difficulty": "difficult",
+        "lengthInMiles": 13.3,
+        "distanceFromCityInMinutes": 120
+      },
+      {
+        "name": "Centennial Cone Trail From Camino Perdido",
+        "difficulty": "difficult",
+        "lengthInMiles": 13.3,
+        "distanceFromCityInMinutes": 120
+      },
+      {
+        "name": "Centennial Cone Trail From Camino Perdido",
+        "difficulty": "difficult",
+        "lengthInMiles": 13.3,
+        "distanceFromCityInMinutes": 120
+      },
+      {
+        "name": "Centennial Cone Trail From Camino Perdido",
+        "difficulty": "difficult",
+        "lengthInMiles": 13.3,
+        "distanceFromCityInMinutes": 120
+      },
+      {
+        "name": "Centennial Cone Trail From Camino Perdido",
+        "difficulty": "difficult",
+        "lengthInMiles": 13.3,
+        "distanceFromCityInMinutes": 120
+      },
+      {
+        "name": "Centennial Cone Trail From Camino Perdido",
+        "difficulty": "difficult",
+        "lengthInMiles": 13.3,
+        "distanceFromCityInMinutes": 120
+      },
+      {
+        "name": "Centennial Cone Trail From Camino Perdido",
+        "difficulty": "difficult",
+        "lengthInMiles": 13.3,
+        "distanceFromCityInMinutes": 120
+      },
+      {
+        "name": "Centennial Cone Trail From Camino Perdido",
+        "difficulty": "difficult",
+        "lengthInMiles": 13.3,
+        "distanceFromCityInMinutes": 120
+      },
+      {
+        "name": "Centennial Cone Trail From Camino Perdido",
+        "difficulty": "difficult",
+        "lengthInMiles": 13.3,
+        "distanceFromCityInMinutes": 120
+      },
+    ],
+
+    boulder: [
+      {
+        "name": "Sanitas",
+        "difficulty": "moderate",
+        "lengthInMiles": 3,
+        "distanceFromCityInMinutes":5
+      },
+      {
+        "name": "Green Mountain",
+        "difficulty": "difficult",
+        "lengthInMiles": 6,
+        "distanceFromCityInMinutes": 5
+      },
+      {
+        "name": "Bear Peak",
+        "difficulty": "difficult",
+        "lengthInMiles": 4,
+        "distanceFromCityInMinutes": 8
+      },
+      {
+        "name": "Marshall Mesa",
+        "difficulty": "easy",
+        "lengthInMiles": 3.3,
+        "distanceFromCityInMinutes": 5
+      },
+      {
+        "name": "South Boulder Peak",
+        "difficulty": "moderate",
+        "lengthInMiles": 7.5,
+        "distanceFromCityInMinutes": 10
+      },
+      {
+        "name": "Centennial Cone Trail From Camino Perdido",
+        "difficulty": "difficult",
+        "lengthInMiles": 13.3,
+        "distanceFromCityInMinutes": 120
+      },
+      {
+        "name": "Centennial Cone Trail From Camino Perdido",
+        "difficulty": "difficult",
+        "lengthInMiles": 13.3,
+        "distanceFromCityInMinutes": 120
+      },
+      {
+        "name": "Centennial Cone Trail From Camino Perdido",
+        "difficulty": "difficult",
+        "lengthInMiles": 13.3,
+        "distanceFromCityInMinutes": 120
+      },
+      {
+        "name": "Centennial Cone Trail From Camino Perdido",
+        "difficulty": "difficult",
+        "lengthInMiles": 13.3,
+        "distanceFromCityInMinutes": 120
+      },
+      {
+        "name": "Centennial Cone Trail From Camino Perdido",
+        "difficulty": "difficult",
+        "lengthInMiles": 13.3,
+        "distanceFromCityInMinutes": 120
+      },
+      {
+        "name": "Centennial Cone Trail From Camino Perdido",
+        "difficulty": "difficult",
+        "lengthInMiles": 13.3,
+        "distanceFromCityInMinutes": 120
+      },
+      {
+        "name": "Centennial Cone Trail From Camino Perdido",
+        "difficulty": "difficult",
+        "lengthInMiles": 13.3,
+        "distanceFromCityInMinutes": 120
+      },
+      {
+        "name": "Centennial Cone Trail From Camino Perdido",
+        "difficulty": "difficult",
+        "lengthInMiles": 13.3,
+        "distanceFromCityInMinutes": 120
+      },
+      {
+        "name": "Centennial Cone Trail From Camino Perdido",
+        "difficulty": "difficult",
+        "lengthInMiles": 13.3,
+        "distanceFromCityInMinutes": 120
+      },
+      {
+        "name": "Centennial Cone Trail From Camino Perdido",
+        "difficulty": "difficult",
+        "lengthInMiles": 13.3,
+        "distanceFromCityInMinutes": 120
+      },
+      {
+        "name": "Centennial Cone Trail From Camino Perdido",
+        "difficulty": "difficult",
+        "lengthInMiles": 13.3,
+        "distanceFromCityInMinutes": 120
+      },
+    ]
+  }
+}
