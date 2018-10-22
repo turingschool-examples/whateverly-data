b61cdca7e1c32bc005fcfa5c6ccb6acdd890c9da
Eric Fitzsimons <ericfitzsimons451@gmail.com>
2018-10-22 11:17:04 -0600

    dataset for ericF

---
+++ sunny-dataset.js
@@ -0,0 +1,708 @@
+const innova = {
+  "speed1": [
+  {
+    "name": "Birdie",
+    "type": "putt & approach",
+    "glide": "2",
+    "turn": "0",
+    "fade": "0"
+  }, 
+  {
+    "name": "Polecat",
+    "type": "putt & approach",
+    "glide": "3",
+    "turn": "0",
+    "fade": "0"
+  },
+  {
+    "name": "Sonic",
+    "type": "putt & approach",
+    "glide": "2",
+    "turn": "-4",
+    "fade": "0"
+  }
+],
+  "speed2": [
+  {
+    "name": "Aviar",
+    "type": "putt & approach",
+    "glide": "3",
+    "turn": "0",
+    "fade": "1"
+  }, 
+  {
+    "name": "Aviar Driver",
+    "type": "putt & approach",
+    "glide": "3",
+    "turn": "0",
+    "fade": "2"
+  },
+  {
+    "name": "Classic Aviar",
+    "type": "putt & approach",
+    "glide": "3",
+    "turn": "0",
+    "fade": "0"
+  },
+  {
+    "name": "JK Aviar",
+    "type": "putt & approach",
+    "glide": "3",
+    "turn": "0",
+    "fade": "2"
+  },
+  {
+    "name": "KC Aviar",
+    "type": "putt & approach",
+    "glide": "3",
+    "turn": "0",
+    "fade": "2"
+  }, 
+  {
+    "name": "Nova",
+    "type": "putt & approach",
+    "glide": "3",
+    "turn": "0",
+    "fade": "0"
+  },
+  {
+    "name": "Rhyno",
+    "type": "putt & approach",
+    "glide": "1",
+    "turn": "0",
+    "fade": "3"
+  },
+  {
+    "name": "Yeti Aviar",
+    "type": "putt & approach",
+    "glide": "3",
+    "turn": "0",
+    "fade": "2"
+  },
+  {
+    "name": "Whale",
+    "type": "putt & approach",
+    "glide": "3",
+    "turn": "0",
+    "fade": "1"
+  }
+],
+  "speed3": [
+  {
+    "name": "Aero",
+    "type": "putt & approach",
+    "glide": "6",
+    "turn": "0",
+    "fade": "0"
+  }, 
+  {
+    "name": "Aviar3",
+    "type": "putt & approach",
+    "glide": "2",
+    "turn": "0",
+    "fade": "2"
+  },
+  {
+    "name": "AviarX3",
+    "type": "putt & approach",
+    "glide": "3",
+    "turn": "0",
+    "fade": "3"
+  },
+  {
+    "name": "Colt",
+    "type": "putt & approach",
+    "glide": "4",
+    "turn": "-1",
+    "fade": "1"
+  },
+  {
+    "name": "Dart",
+    "type": "putt & approach",
+    "glide": "4",
+    "turn": "0",
+    "fade": "0"
+  }, 
+  {
+    "name": "Mirage",
+    "type": "putt & approach",
+    "glide": "4",
+    "turn": "-4",
+    "fade": "0"
+  },
+  {
+    "name": "Hydra",
+    "type": "putt & approach",
+    "glide": "3",
+    "turn": "0",
+    "fade": "2"
+  },
+  {
+    "name": "Pig",
+    "type": "putt & approach",
+    "glide": "1",
+    "turn": "0",
+    "fade": "3"
+  },
+  {
+    "name": "Stud",
+    "type": "putt & approach",
+    "glide": "3",
+    "turn": "0",
+    "fade": "1"
+  },
+  {
+    "name": "XD",
+    "type": "putt & approach",
+    "glide": "4",
+    "turn": "-1",
+    "fade": "1"
+  }
+  ],
+  "speed4": [
+  {
+    "name": "Cobra",
+    "type": "midrange",
+    "glide": "5",
+    "turn": "-2",
+    "fade": "2"
+  }, 
+  {
+    "name": "Coyote",
+    "type": "midrange",
+    "glide": "5",
+    "turn": "0",
+    "fade": "1"
+  },
+  {
+    "name": "King Cobra",
+    "type": "midrange",
+    "glide": "5",
+    "turn": "0",
+    "fade": "2"
+  },
+  {
+    "name": "Mako",
+    "type": "midrange",
+    "glide": "5",
+    "turn": "0",
+    "fade": "0"
+  },
+  {
+    "name": "Rat",
+    "type": "midrange",
+    "glide": "2",
+    "turn": "0",
+    "fade": "3"
+  },
+  {
+    "name": "Roc",
+    "type": "midrange",
+    "glide": "4",
+    "turn": "0",
+    "fade": "3"
+  },
+  {
+    "name": "Roc+",
+    "type": "midrange",
+    "glide": "5",
+    "turn": "0",
+    "fade": "2"
+  }, 
+  {
+    "name": "Shark",
+    "type": "midrange",
+    "glide": "4",
+    "turn": "0",
+    "fade": "2"
+  },
+  {
+    "name": "Stingray",
+    "type": "midrange",
+    "glide": "5",
+    "turn": "-3",
+    "fade": "1"
+  },
+  {
+    "name": "VRoc",
+    "type": "midrange",
+    "glide": "4",
+    "turn": "0",
+    "fade": "1"
+  },
+  {
+    "name": "Wolf",
+    "type": "midrange",
+    "glide": "3",
+    "turn": "-4",
+    "fade": "1"
+  }
+],
+  "speed5": [
+  {
+    "name": "Atlas",
+    "type": "midrange",
+    "glide": "4",
+    "turn": "0",
+    "fade": "1"
+  }, 
+  {
+    "name": "Cro",
+    "type": "midrange",
+    "glide": "3",
+    "turn": "0",
+    "fade": "2"
+  },
+  {
+    "name": "Foxbat",
+    "type": "midrange",
+    "glide": "6",
+    "turn": "-1",
+    "fade": "0"
+  },
+  {
+    "name": "Gator",
+    "type": "midrange",
+    "glide": "2",
+    "turn": "0",
+    "fade": "4"
+  },
+  {
+    "name": "Kite",
+    "type": "midrange",
+    "glide": "6",
+    "turn": "-3",
+    "fade": "1"
+  }, 
+  {
+    "name": "Mako3",
+    "type": "midrange",
+    "glide": "5",
+    "turn": "0",
+    "fade": "0"
+  },
+  {
+    "name": "Manta",
+    "type": "midrange",
+    "glide": "5",
+    "turn": "-2",
+    "fade": "1"
+  },
+  {
+    "name": "Panther",
+    "type": "midrange",
+    "glide": "4",
+    "turn": "-2",
+    "fade": "1"
+  },
+  {
+    "name": "Roc3",
+    "type": "midrange",
+    "glide": "4",
+    "turn": "0",
+    "fade": "3"
+  },
+  {
+    "name": "RocX3",
+    "type": "midrange",
+    "glide": "4",
+    "turn": "0",
+    "fade": "3.5"
+  },
+  {
+    "name": "Shark3",
+    "type": "midrange",
+    "glide": "4",
+    "turn": "0",
+    "fade": "2"
+  },
+  {
+    "name": "Skeeter",
+    "type": "midrange",
+    "glide": "5",
+    "turn": "-1",
+    "fade": "1"
+  },
+  {
+    "name": "Spider",
+    "type": "midrange",
+    "glide": "3",
+    "turn": "0",
+    "fade": "1"
+  },
+  {
+    "name": "VCobra",
+    "type": "midrange",
+    "glide": "5",
+    "turn": "-1",
+    "fade": "2"
+  },
+  {
+    "name": "Wombat",
+    "type": "midrange",
+    "glide": "6",
+    "turn": "-1",
+    "fade": "0"
+  },
+  {
+    "name": "Wombat3",
+    "type": "midrange",
+    "glide": "6",
+    "turn": "-1",
+    "fade": "0"
+  }
+],
+  "speed6": [
+  {
+    "name": "Cheetah",
+    "type": "fairway driver",
+    "glide": "4",
+    "turn": "0",
+    "fade": "1"
+  }, 
+  {
+    "name": "Gazelle",
+    "type": "fairway driver",
+    "glide": "3",
+    "turn": "0",
+    "fade": "2"
+  },
+  {
+    "name": "Leopard",
+    "type": "fairway driver",
+    "glide": "6",
+    "turn": "-1",
+    "fade": "0"
+  },
+  {
+    "name": "Viper",
+    "type": "fairway driver",
+    "glide": "4",
+    "turn": "1",
+    "fade": "5"
+  },
+  {
+    "name": "Whippet",
+    "type": "fairway driver",
+    "glide": "3",
+    "turn": "1",
+    "fade": "5"
+  }
+],
+  "speed7": [
+  {
+    "name": "Banshee",
+    "type": "fairway driver",
+    "glide": "5",
+    "turn": "0",
+    "fade": "3"
+  },
+  {
+    "name": "Eagle",
+    "type": "fairway driver",
+    "glide": "4",
+    "turn": "-1",
+    "fade": "3"
+  },
+  {
+    "name": "Leopard3",
+    "type": "fairway driver",
+    "glide": "5",
+    "turn": "-2",
+    "fade": "1"
+  },
+  {
+    "name": "Teebird",
+    "type": "fairway driver",
+    "glide": "5",
+    "turn": "0",
+    "fade": "2"
+  },
+  {
+    "name": "TL",
+    "type": "fairway driver",
+    "glide": "5",
+    "turn": "0",
+    "fade": "1"
+  }
+],
+  "speed8": [
+  {
+    "name": "Archangel",
+    "type": "fairway driver",
+    "glide": "6",
+    "turn": "-4",
+    "fade": "1"
+  },
+  {
+    "name": "Dragon",
+    "type": "fairway driver",
+    "glide": "5",
+    "turn": "-2",
+    "fade": "2"
+  },
+  {
+    "name": "Teebird3",
+    "type": "fairway driver",
+    "glide": "4",
+    "turn": "0",
+    "fade": "2"
+  },
+  {
+    "name": "TL3",
+    "type": "fairway driver",
+    "glide": "4",
+    "turn": "0",
+    "fade": "1"
+  }
+],
+  "speed9": [
+  {
+    "name": "Firebird",
+    "type": "distance driver",
+    "glide": "3",
+    "turn": "0",
+    "fade": "4"
+  },
+  {
+    "name": "FL",
+    "type": "distance driver",
+    "glide": "3",
+    "turn": "0",
+    "fade": "2"
+  },
+  {
+    "name": "Roadrunner",
+    "type": "distance driver",
+    "glide": "5",
+    "turn": "-4",
+    "fade": "1"
+  },
+  {
+    "name": "Savant",
+    "type": "distance driver",
+    "glide": "5",
+    "turn": "-1",
+    "fade": "2"
+  },
+  {
+    "name": "Sidewinder",
+    "type": "distance driver",
+    "glide": "5",
+    "turn": "-3",
+    "fade": "1"
+  },
+  {
+    "name": "Thunderbird",
+    "type": "distance driver",
+    "glide": "5",
+    "turn": "0",
+    "fade": "2"
+  },
+   {
+    "name": "Valkyrie",
+    "type": "distance driver",
+    "glide": "4",
+    "turn": "-2",
+    "fade": "2"
+  },
+  {
+    "name": "Viking",
+    "type": "distance driver",
+    "glide": "4",
+    "turn": "-1",
+    "fade": "2"
+  }
+],
+  "speed10": [
+  {
+    "name": "Beast",
+    "type": "distance driver",
+    "glide": "5",
+    "turn": "-2",
+    "fade": "2"
+  },
+  {
+    "name": "Monarch",
+    "type": "distance driver",
+    "glide": "5",
+    "turn": "-4",
+    "fade": "1"
+  },
+  {
+    "name": "Monster",
+    "type": "distance driver",
+    "glide": "3",
+    "turn": "0",
+    "fade": "5"
+  },
+  {
+    "name": "Orc",
+    "type": "distance driver",
+    "glide": "4",
+    "turn": "-1",
+    "fade": "3"
+  },
+  {
+    "name": "Starfire",
+    "type": "distance driver",
+    "glide": "4",
+    "turn": "0",
+    "fade": "3"
+  }
+],
+  "speed11": [
+  {
+    "name": "Archon",
+    "type": "distance driver",
+    "glide": "5",
+    "turn": "-2",
+    "fade": "2"
+  },
+  {
+    "name": "Krait",
+    "type": "distance driver",
+    "glide": "5",
+    "turn": "-1",
+    "fade": "2"
+  },
+  {
+    "name": "Mamba",
+    "type": "distance driver",
+    "glide": "6",
+    "turn": "-5",
+    "fade": "1"
+  },
+  {
+    "name": "Max",
+    "type": "distance driver",
+    "glide": "3",
+    "turn": "0",
+    "fade": "5"
+  },
+  {
+    "name": "Mystere",
+    "type": "distance driver",
+    "glide": "4",
+    "turn": "0",
+    "fade": "4"
+  },
+  {
+    "name": "Teerex",
+    "type": "distance driver",
+    "glide": "5",
+    "turn": "0",
+    "fade": "2"
+  },
+   {
+    "name": "Wraith",
+    "type": "distance driver",
+    "glide": "5",
+    "turn": "-1",
+    "fade": "3"
+  }
+],
+  "speed12": [
+  {
+    "name": "Destroyer",
+    "type": "distance driver",
+    "glide": "5",
+    "turn": "-1",
+    "fade": "3"
+  }
+  {
+    "name": "Teedevil",
+    "type": "distance driver",
+    "glide": "5",
+    "turn": "-1",
+    "fade": "2"
+  },
+   {
+    "name": "Tern",
+    "type": "distance driver",
+    "glide": "6",
+    "turn": "-3",
+    "fade": "2"
+  },
+  {
+    "name": "Wahoo",
+    "type": "distance driver",
+    "glide": "6",
+    "turn": "-2",
+    "fade": "2"
+    },
+  {
+    "name": "XCaliber",
+    "type": "distance driver",
+    "glide": "5",
+    "turn": "0",
+    "fade": "4"
+  }
+],  "speed13": [
+  {
+    "name": "Ape",
+    "type": "distance driver",
+    "glide": "5",
+    "turn": "0",
+    "fade": "4"
+  },
+  {
+    "name": "Boss",
+    "type": "distance driver",
+    "glide": "5",
+    "turn": "-1",
+    "fade": "3"
+  },
+   {
+    "name": "Daedalus",
+    "type": "distance driver",
+    "glide": "6",
+    "turn": "-3",
+    "fade": "2"
+  },
+  {
+    "name": "Dominator",
+    "type": "distance driver",
+    "glide": "5",
+    "turn": "-1",
+    "fade": "2"
+    },
+  {
+    "name": "Groove",
+    "type": "distance driver",
+    "glide": "6",
+    "turn": "-2",
+    "fade": "2"
+  },
+   {
+    "name": "Katana",
+    "type": "distance driver",
+    "glide": "5",
+    "turn": "-3",
+    "fade": "3"
+  },
+   {
+    "name": "Shryke",
+    "type": "distance driver",
+    "glide": "6",
+    "turn": "-2",
+    "fade": "2"
+  },
+  {
+    "name": "Vulcan",
+    "type": "distance driver",
+    "glide": "5",
+    "turn": "-4",
+    "fade": "2"
+    } 
+],
+  "speed14": [
+  {
+    "name": "Colossus",
+    "type": "distance driver",
+    "glide": "5",
+    "turn": "-2",
+    "fade": "3"
+  }
+]
+};
