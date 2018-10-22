28c75e95062ac945a5ad891b05ea4cdd4f08f8c0
Libby Yeh <33442154+libbyeh@users.noreply.github.com>
2018-10-21 20:44:25 -0600

    Add dataset

---
+++ rest-data-set.js
@@ -0,0 +1,291 @@
+const restData = {
+    features: {
+      to-try: 1,
+      favorite: 1
+  },
+
+  restaurants-try: {
+    to-try: {
+      restaurants_bank_1 [
+      {
+        name: "Minhas Micro Brewery",
+        type: "Pizza",
+        neighborhood: "Lohi",
+        stars: 4,
+        reviews: 300,
+      },
+      {
+        name: "CK'S BBQ & Catering",
+        type: "BBQ",
+        neighborhood: "Sunnyside",
+        stars: 3,
+        reviews: 360,
+      },
+      {
+        name: "La Bastringue",
+        type: "French",
+        neighborhood: "Larimer",
+        stars: 4,
+        reviews: 500,
+      },
+      {
+        name: "Thai One On",
+        type: "Thai",
+        neighborhood: "Sunnyside",
+        stars: 3,
+        reviews: 700,
+      },
+      {
+        name: "Sushi 8",,
+        type: "Japanese",
+        neighborhood: "RINO",
+        stars: 4,
+        reviews: 200,
+      },
+      {
+        name: "Rally's Hamburgers",
+        type: "American",
+        neighborhood: "Uptown",
+        stars: 3,
+        reviews: 100,
+      },
+      {
+        name: "Mysore Indian Cuisine",
+        type: "Indian",
+        neighborhood: "Alameda",
+        stars: 3,
+        reviews: 450,
+      },
+      {
+        name: "Mihana",,
+        type: "Japanese",
+        neighborhood: "Larimer",
+        stars: 4,
+        reviews: 490,
+      },
+      {
+        name: "Rib Shop",
+        type: "American",
+        neighborhood: "Greenwood Village",
+        stars: 3,
+        reviews: 550,
+      },
+      {
+        name: "Citi Trends",
+        type: "Breakfast",
+        neighborhood: "RINO",
+        stars: 4,
+        reviews: 349,
+      },
+      {
+        name: "Los Toros Numero 2"
+        type: "Mexican",
+        neighborhood: "Uptown",
+        stars: 3,
+        reviews: 890,
+      },
+           {
+        name: "Original Hamburger Works",
+        type: "American",
+        neighborhood: "Stapelton",
+        stars: 3,
+        reviews: 203,
+      },
+           {
+        name: "Teresas Pizzeria",
+        type: "Pizza",
+        neighborhood: "Berkley",
+        stars: 4,
+        reviews: 700,
+      },
+           {
+        name: "Omelet House",
+        type: "Breakfast",
+        neighborhood: "RINO",
+        stars: 3,
+        reviews: 600,
+      },
+           {
+        name: "OneFold",,
+        type: "Asian",
+        neighborhood: "Uptown",
+        stars: 5,
+        reviews: 560,
+      },
+           {
+        name: "Pho Sure",,
+        type: "Vietnamese",
+        neighborhood: "Federal",
+        stars: 4,
+        reviews: 592,
+      },
+      {
+        name: "Southern Accent Restaurant",
+        type: "Southern",
+        neighborhood: "Sunnyside",
+        stars: 4,
+        reviews: 200,
+      },
+      {
+        name: "Lao Wang",,
+        type: "Taiwanese",
+        neighborhood: "Federal",
+        stars: 5,
+        reviews: 1000,
+      },
+      {
+        name: "Empanadas Express",
+        type: "Venezuelan",
+        neighborhood: "Sunnyside",
+        stars: 4,
+        reviews: 670,
+      },
+      {
+        name: "Tacos Veloz",
+        type: "Mexican",
+        neighborhood: "Federal",
+        stars: 3,
+        reviews: 349,
+      },
+      {
+        name: "Osteria De Marco",
+        type: "Italian",
+        neighborhood: "Larimer",
+        stars: 4,
+        reviews: 849,
+      },
+      {
+        name: "Byblos Cafe",
+        type: "Greek",
+        neighborhood: "City Park",
+        stars: 3,
+        reviews: 569,
+      },
+      {
+        name: "Alloy Thai",
+        type: "Thai",
+        neighborhood: "Ballpark",
+        stars: 4,
+        reviews: 1149,
+      },
+      {
+        name: "Caviletti",
+        type: "Italian",
+        neighborhood: "Lohi",
+        stars: 4,
+        reviews: 156,
+      },
+      {
+        name: "Tofu House",
+        type: "Vegan",
+        neighborhood: "Ballpark",
+        stars: 3,
+        reviews: 480,
+      },
+      {
+        name: "White Pie",
+        type: "Pizza",
+        neighborhood: "Berkley",
+        stars: 3,
+        reviews: 363,
+      },
+      {
+        name: "Menya Ramen",
+        type: "Japanese",
+        neighborhood: "Ballpark",
+        stars: 4,
+        reviews: 49,
+      },
+      {
+        name: "Aki Hotpot",,
+        type: "Chinese",
+        neighborhood: "City Park",
+        stars: 3,
+        reviews: 209,
+      },
+
+
+
+      ],
+    }
+  }
+
+
+  restaurants-love: {
+    favorite: {
+      restaurants_bank_2 [
+      {
+        name: "Cuba Cuba",
+        type: "Cuban",
+        neighborhood: "Sunnyside",
+        stars: 4,
+        reviews: 380,
+      },
+      {
+        name: "Cafe Brazil",
+        type: "Brazilian",
+        neighborhood: "Sunnyside",
+        stars: 4,
+        reviews: 860,
+      },
+      {
+        name: "Tortas ATM",
+        type: "Mexican",
+        neighborhood: "Federal",
+        stars: 3,
+        reviews: 540,
+      },
+      {
+        name: "New Saigon",
+        type: "Vietnamese",
+        neighborhood: "Federal",
+        stars: 4,
+        reviews: 700,
+      },
+      {
+        name: "Star Kitchen",,
+        type: "Chinese",
+        neighborhood: "Federal",
+        stars: 4,
+        reviews: 890,
+      },
+      {
+        name: "Ethiopia",
+        type: "Ethiopian",
+        neighborhood: "Colfax",
+        stars: 4,
+        reviews: 100,
+      },
+      {
+        name: "Yak and Yeti",
+        type: "Indian",
+        neighborhood: "Arvada",
+        stars: 4,
+        reviews: 450,
+      },
+      {
+        name: "Potager",,
+        type: "American",
+        neighborhood: "Uptown",
+        stars: 4,
+        reviews: 498,
+      },
+      {
+        name: "Cart Driver",
+        type: "Pizza",
+        neighborhood: "RINO",
+        stars: 4,
+        reviews: 553,
+      },
+      {
+        name: "New Delhi",
+        type: "Indian",
+        neighborhood: "RINO",
+        stars: 4,
+        reviews: 323,
+      },
+      ],
+    }
+}
+
+module.exports = restData;
