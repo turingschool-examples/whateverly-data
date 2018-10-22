5a1d823a8d353c4037729241e534290f14f63b2a
Loryn Mason <masonloryn@gmail.com>
2018-10-22 08:14:05 -0600

    Add harry potter dataset

---
+++ datasets/harryPotter.js
@@ -0,0 +1,229 @@
+let harryPotterCharacters = [
+  {
+    name: 'Hermione Granger',
+    house: 'Gryffindor',
+    bloodStatus: 'Muggle-born',
+    wand: ['10¾ vine wood dragon heartstring', '12¾" walnut dragon heartstring'],
+    patronus: 'Otter'
+  },
+  {
+    name: 'Harry Potter',
+    house: 'Gryffindor',
+    bloodStatus: 'Half-blood',
+    wand: ['11 Holly phoenix feather', '10¾ Vine dragon heartstring', '10 Blackthorn unknown core', '15 Elder thestral hair'],
+    patronus: 'Stag'
+  },
+  {
+    name: 'Ronald Weasley',
+    house: 'Gryffindor',
+    bloodStatus: 'Pure-blood',
+    wand: ['12 Ash unicorn tail hair', '14 Willo unicorn tail hair', '9¼ Chestnut Dragon heartstring'],
+    patronus: 'Jack Russell Terrier'
+  },
+  {
+    name: 'Luna Lovegood',
+    house: 'Ravenclaw',
+    bloodStatus: 'Pure-blood',
+    wand: [],
+    patronus: 'Hare'
+  },
+  {
+    name: 'Cedric Diggory',
+    house: 'Hufflepuff',
+    bloodStatus: 'Pure-blood',
+    wand: ['12¼ Ash unicorn hair'],
+    patronus: null
+  },
+  {
+    name: 'Draco Malfoy',
+    house: 'Slytherin',
+    bloodStatus: 'Pure-blood',
+    wand: ['10 Hawthorn unicorn hair', '18 Elm dragon heartstring'],
+    patronus: null
+  },
+  {
+    name: 'Pansy Parkinson',
+    house: 'Slytherin',
+    bloodStatus: 'Pure-blood',
+    wand: [],
+    patronus: null
+  },
+  {
+    name: 'Cho Chang',
+    house: 'Ravenclaw',
+    bloodStatus: 'Half-blood',
+    wand: [],
+    patronus: 'Swan'
+  },
+   {
+    name: 'Ginevra Weasley',
+    house: 'Gryffindor',
+    bloodStatus: 'Pure-blood',
+    wand: ['Unknown length Yew unknown core'],
+    patronus: 'Horse'
+  },
+   {
+    name: 'Neville Longbottom',
+    house: 'Gryffindor',
+    bloodStatus: 'Pure-blood',
+    wand: ['13 Cherry unicorn hair'],
+    patronus: 'Non-corporeal'
+  },
+   {
+    name: 'Colin Creevey',
+    house: 'Gryffindor',
+    bloodStatus: 'Muggle-born',
+    wand: [],
+    patronus: null
+  },
+   {
+    name: 'Zacharias Smith',
+    house: 'Hufflepuff',
+    bloodStatus: 'Half-blood',
+    wand: [],
+    patronus: null
+  },
+   {
+    name: 'Lavender Brown',
+    house: 'Gryffindor',
+    bloodStatus: 'Pure-blood',
+    wand: [],
+    patronus: 'Non-corporea'
+  },
+   {
+    name: 'Parvati Patil',
+    house: 'Ravenclaw',
+    bloodStatus: 'Half-blood',
+    wand: [],
+    patronus: null
+  },
+   {
+    name: 'Vincent Crabbe',
+    house: 'Slytherin',
+    bloodStatus: 'Pure-blood',
+    wand: [],
+    patronus: null
+  },
+   {
+    name: 'Gregory Goyle',
+    house: 'Slytherin',
+    bloodStatus: 'Pure-blood',
+    wand: [],
+    patronus: null
+  },
+   {
+    name: 'Blaise Zabini',
+    house: 'Slytherin',
+    bloodStatus: 'Half-blood',
+    wand: [],
+    patronus: null
+  },
+   {
+    name: 'Daphne Greengrass',
+    house: 'Slytherin',
+    bloodStatus: 'Pure-blood',
+    wand: [],
+    patronus: null
+  },
+   {
+    name: 'Penelope Clearwater',
+    house: 'Ravenclaw',
+    bloodStatus: 'Muggle-born',
+    wand: [],
+    patronus: null
+  },
+   {
+    name: 'Millicent Bulstrode',
+    house: 'Slytherin',
+    bloodStatus: 'Half-blood',
+    wand: [],
+    patronus: null
+  },
+   {
+    name: 'Justin Finch-Fletchley',
+    house: 'Hufflepuff',
+    bloodStatus: 'Muggle-born',
+    wand: [],
+    patronus: null
+  },
+   {
+    name: 'Susan Bones',
+    house: 'Hufflepuff',
+    bloodStatus: 'Half-blood',
+    wand: [],
+    patronus: null
+  },
+   {
+    name: 'Ernest Macmillan',
+    house: 'Hufflepuff',
+    bloodStatus: 'Pure-blood',
+    wand: [],
+    patronus: 'Boar'
+  },
+   {
+    name: 'Hannah Abbott',
+    house: 'Hufflepuff',
+    bloodStatus: 'Half-blood',
+    wand: [],
+    patronus: null
+  },
+   {
+    name: 'Nymphadora Tonks',
+    house: 'Hufflepuff',
+    bloodStatus: 'Half-blood',
+    wand: [],
+    patronus: 'wolf'
+  },
+   {
+    name: 'Remus Lupin',
+    house: 'Gryffindor',
+    bloodStatus: 'Half-blood',
+    wand: ['10¼ Cypress unicorn hair'],
+    patronus: 'wolf'
+  },
+   {
+    name: 'Sirius Black',
+    house: 'Gryffindor',
+    bloodStatus: 'Pure-blood',
+    wand: [],
+    patronus: null
+  },
+   {
+    name: 'James Potter',
+    house: 'Gryffindor',
+    bloodStatus: 'Pure-blood',
+    wand: ['11 Mahogany unknown core'],
+    patronus: 'Stag'
+  },
+   {
+    name: 'Lily J. Potter',
+    house: 'Gryffindor',
+    bloodStatus: 'Muggle-born',
+    wand: ['10¼ Willow unknown core'],
+    patronus: 'doe'
+  },
+   {
+    name: 'Albus Dumbledore',
+    house: 'Gryffindor',
+    bloodStatus: 'Half-blood',
+    wand: ['15 Elder Thestral tail hair core'],
+    patronus: 'phoenix'
+  },
+   {
+    name: 'Fred Weasley',
+    house: 'Gryffindor',
+    bloodStatus: 'Pure-blood',
+    wand: [],
+    patronus: null
+  },
+   {
+    name: 'George Weasley',
+    house: 'Gryffindor',
+    bloodStatus: 'Half-blood',
+    wand: [],
+    patronus: null
+  },
+]
+
+
+module.exports = harryPotterCharacters
